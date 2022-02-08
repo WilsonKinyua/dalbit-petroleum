8376c31bcec17d8cbc1adfd0868129b5ca967453
WilsonKinyua <wilsonkinyuam@gmail.com>
2022-02-08 13:40:30 +0300

    division section done

--- resources/views/public/division.blade.php
+++ resources/views/public/division.blade.php
@@ -5,13 +5,14 @@
 @section('description') {!! $division->country_description ?? '' !!} @endsection
 
 @section('css')
-<style>
-    .service-item .service-img {
-        width: 100%;
-        padding: 20px;
-        background-color: white;
-    }
-</style>
+    <style>
+        .service-item .service-img {
+            width: 100%;
+            padding: 20px;
+            background-color: white;
+        }
+
+    </style>
 @endsection
 @section('content')
     <div class="inner-header">
@@ -80,112 +81,149 @@
                         </div>
                     </div>
                 </div>
+                @if ($division->transport_image === null && $division->infrastructure_storage_image === null)
+                    @if ($division->operation_image)
+                        <div class="col-md-4 align-self-center mb-md-0 mb-4 mt-5">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img class="img-fluid " src="{{ $division->operation_image->getUrl() }}" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-8 align-self-center pl-3 pl-lg-5 mt-5">
+                            <div class="row">
+                                <div class="col-md-12 col-lg-12 col-xl-12">
+                                    <div class="tab-content" id="v-pills-tabContent">
+                                        <div class="tab-pane fade show active" id="v-pills-who-we-are" role="tabpanel"
+                                            aria-labelledby="v-pills-who-we-are-tab">
+                                            <div class="row">
+                                                <div class="col-lg-12 mb-lg-0 mb-4">
+                                                    <h6 class="mb-3">Operations</h6>
+                                                    <p>
+                                                        {!! $division->operation_description ?? '' !!}
+                                                    </p>
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                @endif
             </div>
         </div>
     </section>
     <section class="space-ptb">
         <div class="container">
             <div class="row justify-content-start">
-                @if ($division->operation_image)
-                    <div class="col-sm-6 col-lg-4 mb-4 operations-img">
-                        <div class="service-item">
-                            <div class="service-img">
-                                <img class="img-fluid" src="{{ $division->operation_image->getUrl() }}" alt="">
-                                <a data-toggle="modal" data-target="#operationsModal"><i
-                                        class="fas fa-long-arrow-alt-right"></i></a>
-                            </div>
-                            <div class="service-info">
-                                <h6 class="service-info-title"><a data-toggle="modal"
-                                        data-target="#operationsModal">Operations</a></h6>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- OPerations Modal -->
-                    <div class="modal fade" id="operationsModal" tabindex="-1" role="dialog"
-                        aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-                            <div class="modal-content">
-                                <div class="mr-3 mt-3">
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
+                @if ($division->operation_image != null && $division->transport_image != null && $division->infrastructure_storage_image != null)
+                    @if ($division->operation_image)
+                        <div class="col-sm-6 col-lg-4 mb-4 operations-img">
+                            <div class="service-item">
+                                <div class="service-img">
+                                    <img class="img-fluid" src="{{ $division->operation_image->getUrl() }}" alt="">
+                                    <a data-toggle="modal" data-target="#operationsModal"><i
+                                            class="fas fa-long-arrow-alt-right"></i></a>
                                 </div>
-                                <div class="modal-body">
-                                    <p>
-                                        {!! $division->operation_description ?? '' !!}
-                                    </p>
+                                <div class="service-info">
+                                    <h6 class="service-info-title"><a data-toggle="modal"
+                                            data-target="#operationsModal">Operations</a></h6>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                        <!-- OPerations Modal -->
+                        <div class="modal fade" id="operationsModal" tabindex="-1" role="dialog"
+                            aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                                <div class="modal-content">
+                                    <div class="mr-3 mt-3">
+                                        <button type="button" class="close" data-dismiss="modal"
+                                            aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>
+                                            {!! $division->operation_description ?? '' !!}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                    @if ($division->transport_image)
+                        <div class="col-sm-6 col-lg-4 mb-4">
+                            <div class="service-item">
+                                <div class="service-img">
+                                    <img class="img-fluid" src="{{ $division->transport_image->getUrl() }}" alt="">
+                                    <a data-toggle="modal" data-target="#transportModal"><i
+                                            class="fas fa-long-arrow-alt-right"></i></a>
+                                </div>
+                                <div class="service-info">
+                                    <h6 class="service-info-title"><a href="#">Transport</a></h6>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Transport Modal -->
+                        <div class="modal fade" id="transportModal" tabindex="-1" role="dialog"
+                            aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                                <div class="modal-content">
+                                    <div class="mr-3 mt-3">
+                                        <button type="button" class="close" data-dismiss="modal"
+                                            aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>
+                                            {!! $division->transport_description ?? '' !!}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                    @if ($division->infrastructure_storage_image)
+                        <div class="col-sm-6 col-lg-4 mb-4">
+                            <div class="service-item">
+                                <div class="service-img">
+                                    <img class="img-fluid"
+                                        src="{{ $division->infrastructure_storage_image->getUrl() }}" alt="">
+                                    <a data-toggle="modal" data-target="#infrastructure"><i
+                                            class="fas fa-long-arrow-alt-right"></i></a>
+                                </div>
+                                <div class="service-info">
+                                    <h6 class="service-info-title"><a data-toggle="modal"
+                                            data-target="#infrastructure">Infrastructure &
+                                            Storage</a></h6>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Infrastructure Modal -->
+                        <div class="modal fade" id="infrastructure" tabindex="-1" role="dialog"
+                            aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered" role="document">
+                                <div class="modal-content">
+                                    <div class="mr-3 mt-3">
+                                        <button type="button" class="close" data-dismiss="modal"
+                                            aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>
+                                            {!! $division->infrastructure_storage_description ?? '' !!}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
                 @endif
-                @if ($division->transport_image)
-                    <div class="col-sm-6 col-lg-4 mb-4">
-                        <div class="service-item">
-                            <div class="service-img">
-                                <img class="img-fluid" src="{{ $division->transport_image->getUrl() }}" alt="">
-                                <a data-toggle="modal" data-target="#transportModal"><i
-                                        class="fas fa-long-arrow-alt-right"></i></a>
-                            </div>
-                            <div class="service-info">
-                                <h6 class="service-info-title"><a href="#">Transport</a></h6>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Transport Modal -->
-                    <div class="modal fade" id="transportModal" tabindex="-1" role="dialog"
-                        aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-                            <div class="modal-content">
-                                <div class="mr-3 mt-3">
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    <p>
-                                        {!! $division->transport_description ?? '' !!}
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                @endif
-                @if ($division->infrastructure_storage_image)
-                    <div class="col-sm-6 col-lg-4 mb-4">
-                        <div class="service-item">
-                            <div class="service-img">
-                                <img class="img-fluid" src="{{ $division->infrastructure_storage_image->getUrl() }}"
-                                    alt="">
-                                <a data-toggle="modal" data-target="#infrastructure"><i
-                                        class="fas fa-long-arrow-alt-right"></i></a>
-                            </div>
-                            <div class="service-info">
-                                <h6 class="service-info-title"><a data-toggle="modal"
-                                        data-target="#infrastructure">Infrastructure &
-                                        Storage</a></h6>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Infrastructure Modal -->
-                    <div class="modal fade" id="infrastructure" tabindex="-1" role="dialog"
-                        aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered" role="document">
-                            <div class="modal-content">
-                                <div class="mr-3 mt-3">
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    <p>
-                                        {!! $division->infrastructure_storage_description ?? '' !!}
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                @endif
+
                 @if (!$division->contacts == '')
                     <div class="col-lg-4"></div>
                     <div class="col-lg-6">
@@ -235,27 +273,3 @@
     </section>
 
 @endsection
-{{-- @section('scripts')
-    <script>
-        document.addEventListener('wheel', function(event) {
-            //only vertical scroll
-            if (event.deltaY > 0) {
-                event.preventDefault();
-                smoothScroll(document.documentElement, 100, 1000)
-            }
-        })
-
-        function smoothScroll(domElement, pixel, delay) {
-            const intervalToRepeat = 25;
-            const step = (intervalToRepeat * pixel) / delay;
-            if (step < pixel) {
-                domElement.scrollTop += step;
-                setTimeout(function() {
-                    smoothScroll(domElement, pixel - step, delay)
-                }, intervalToRepeat);
-            }
-
-
-        }
-    </script>
-@endsection --}}
